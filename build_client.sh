aae80dc6203064a362b3c72b8cd94b0d5e6eaba6
kethansrinivas9 <kethansrinivas9@gmail.com>
2020-03-31 19:56:04 -0300

    Added the following changes: * Added individual scripts for genearting images of client and the server * Added changes for storing the user passwords as a SHA256 hash in the db * Added some packages in the requirements.txt for the server image

--- build_client.sh
+++ build_client.sh
@@ -19,34 +19,17 @@ cp "$CURR_DIR/docker_files/client.dockerfile" "$CURR_DIR/dist"
 cd "$CURR_DIR/dist"
 
 echo -e "${GREEN}Building client image...${NO_COLOR}"
-docker build -t daltourism/client:latest -f ./client.dockerfile .
+docker build --no-cache -t daltourism/client:latest -f ./client.dockerfile .
 cd ..
 rm "$CURR_DIR/dist/client.dockerfile"
 
 echo -e "${GREEN}Running the client container...${NO_COLOR}"
 docker run -d --name client -p 7000:7000 daltourism/client
 
-echo -e "${GREEN}Removing old server container...${NO_COLOR}"
-docker rm -f server
-echo -e "${GREEN}Removing old server images...${NO_COLOR}"
-docker rmi -f daltourism/server:latest
 
-echo -e "${GREEN}Preparing files for building server image...${NO_COLOR}"
-cd "$CURR_DIR"
-cp "$CURR_DIR/docker_files/server.dockerfile" "$CURR_DIR/server/"
-cd "$CURR_DIR/server"
-
-echo -e "${GREEN}Building Server image...${NO_COLOR}"
-docker build -t daltourism/server:latest -f ./server.dockerfile .
-cd ..
-rm "$CURR_DIR/server/server.dockerfile"
-
-echo -e "${GREEN}Running the Server container...${NO_COLOR}"
-docker run -d --name server -p 5000:5000 daltourism/server
 
 echo -e "${GREEN}Logging to the Docker Hub...${NO_COLOR}"
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 
 echo -e "${GREEN}Pushing the images to Docker Hub...${NO_COLOR}"
-docker push daltourism/server:latest
 docker push daltourism/client:latest
